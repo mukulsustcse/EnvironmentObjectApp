5311d2f33351c4fd2feb9111eff7b34f5af437fd
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-01 13:36:43 +0200

    Initial Commit

---
+++ environmentObjectApp/environmentObjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  environmentObjectAppApp.swift
+//  environmentObjectApp
+//
+//  Created by M. A. Alim Mukul on 01.09.22.
+//
+
+import SwiftUI
+
+@main
+struct environmentObjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
